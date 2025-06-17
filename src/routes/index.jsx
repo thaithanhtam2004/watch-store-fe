bfb1ac944b14a3a4cc6bd8339fa8a35ab9c47181
PandaBoo <thuyenle1307@gmail.com>
2025-06-17 11:17:08 +0700

    gio hang

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,15 +8,9 @@ import AdminPage from "../components/pages/AdminPage";
 import BestsellerPage from "@/components/pages/BestsellerPage";
 import DongHoNamPage from "@/components/pages/DongHoNamPage";
 import DongHoNuPage from "@/components/pages/DongHoNuPage";
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from '@/components/pages/HomePage';
-import RegisterPage from '@/components/pages/RegisterPage';
-import LoginPage from '@/components/pages/LoginPage';
-import AdminPage from '@/components/pages/AdminPage';
 import ProductDetail from '@/components/pages/ProductDetail';
 import ProductLayout from '@/components/layouts/ProductLayout'; // Import tại đây
-
+import GioHangPage from '@/components/pages/GioHangPage';
 
 const AppRoutes = () => (
   <Routes>
@@ -24,6 +18,7 @@ const AppRoutes = () => (
     <Route path="/register" element={<RegisterPage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/admin" element={<AdminPage />} />
+    <Route path="/cart" element={<GioHangPage />} />
     <Route path="/bestseller" element={<BestsellerPage />} />
     <Route path="/donghonam" element={<DongHoNamPage />} />
     <Route path="/donghonu" element={<DongHoNuPage />} />
